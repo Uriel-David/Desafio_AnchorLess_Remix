3becd6e7d442de69bcfe6c172a13ef33fdba9afc
Uriel-David <uriel.dqaa@gmail.com>
2025-07-13 20:13:30 +0100

    refactor: change API url to a env var

--- app/http/api/visaDossier.ts
+++ app/http/api/visaDossier.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { VisaDocumentApiResponseGrouped, VisaDocumentApiResponseUnity } from '../types/VisaDocumentApiResponse';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: process.env.API_URL,
 });
 
 export const uploadFile = (file: File, type: string, tag: string): Promise<VisaDocumentApiResponseUnity> => {
@@ -11,11 +11,32 @@ export const uploadFile = (file: File, type: string, tag: string): Promise<VisaD
   formData.append('type', type);
   formData.append('tag', tag);
 
-  return api.post<VisaDocumentApiResponseUnity>('/documents/upload', formData).then(result => result.data).catch(e => e);
-};
+  return api.post<VisaDocumentApiResponseUnity>('/documents/upload', formData).then(result => result.data).catch((error) => {
+    if (axios.isAxiosError(error)) {
+      const message = error.response?.data?.message || error.message;
+      return Promise.reject(new Error(message));
+    }
 
-export const fetchFiles = (): Promise<VisaDocumentApiResponseGrouped> => api.get<VisaDocumentApiResponseGrouped>('/documents/list').then(result => result.data).catch(e => e);
+    return Promise.reject(new Error("Erro inesperado ao chamar API"));
+  });
+}
+
+export const fetchFiles = (): Promise<VisaDocumentApiResponseGrouped> => api.get<VisaDocumentApiResponseGrouped>('/documents/list').then(result => result.data).catch((error) => {
+  if (axios.isAxiosError(error)) {
+    const message = error.response?.data?.message || error.message;
+    return Promise.reject(new Error(message));
+  }
+
+  return Promise.reject(new Error("Erro inesperado ao chamar API"));
+});
 
 export const deleteFile = (id: number): Promise<VisaDocumentApiResponseUnity> => api.delete<VisaDocumentApiResponseUnity>('/documents/delete', {
   data: { id },
-}).then(result => result.data).catch(e => e);
+}).then(result => result.data).catch((error) => {
+  if (axios.isAxiosError(error)) {
+    const message = error.response?.data?.message || error.message;
+    return Promise.reject(new Error(message));
+  }
+
+  return Promise.reject(new Error("Erro inesperado ao chamar API"));
+});
